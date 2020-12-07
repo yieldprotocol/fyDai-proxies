1afb32a7f94974d7150fff62c846a4cf2d2b2ff7
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-12-07 21:12:04 +0000

    feat: simple implementation drafted

--- contracts/RollProxy.sol
+++ contracts/RollProxy.sol
@@ -11,6 +11,7 @@ import "./interfaces/IPool.sol";
 import "./helpers/DecimalMath.sol";
 import "./helpers/SafeCast.sol";
 import "./helpers/YieldAuth.sol";
+import "@nomiclabs/buidler/console.sol";
 
 
 contract RollProxy is DecimalMath {
@@ -47,90 +48,71 @@ contract RollProxy is DecimalMath {
 
 
     // Migrate assumes that the series is the same in both pools
-    function migrateLiquidity(IPool pool1, IPool pool2, uint256 poolTokens) public returns (uint256) {
+    function migrateLiquidity(IPool pool1, IPool pool2, uint256 poolTokens) public returns (uint256 minted) {
         IFYDai fyDai = pool1.fyDai();
         require(fyDai == pool2.fyDai(), "RollProxy: Migrate between same series");
 
-        // First we need to know the dai/fyDai balances of pool1 and pool2.
-        // If pool1 dai/fyDai > pool2 dai/fyDai we will sell dai for fyDai in pool2
-        // If pool1 dai/fyDai < pool2 dai/fyDai we will buy dai for fyDai in pool1
         uint256 daiReserves = dai.balanceOf(address(pool2));
         uint256 fyDaiReserves = fyDai.balanceOf(address(pool2));
 
-        // Z1 = pool 1 dai reserves
-        // Y1 = pool 1 fyDai reserves
-        // Z2 = pool 2 dai reserves
-        // Y2 = pool 2 fyDai reserves
-        // z1 = dai obtained from burn
-        // y1 = fyDai obtained from burn
-        // z2 = dai required to mint
-        // y2 = fyDai required to mint
-
-        // z1 = p * (Z1 / s)                 : Obtained from burn
-        // y1 = p * (Y1 / s)                 : Obtained from burn
-        // z2 = ((z1 + y1) * Z2) / (Y2 + Z2) 
-        // y2 = ((z1 + y1) - z2)             : Or, y2 = ((z1 + y1) * Y2) / (Y2 + Z2)
-
         // remove liquidity
         (uint256 daiObtained, uint256 fyDaiObtained) = pool1.burn(msg.sender, address(this), poolTokens);
+        uint256 daiProportion1 = daiObtained.div(daiObtained.add(fyDaiObtained));
+        uint256 daiProportion2 = daiReserves.div(daiReserves.add(fyDaiReserves));
 
-        uint256 daiToAdd = (daiObtained.add(fyDaiObtained)).mul(daiReserves).div(fyDaiReserves.add(daiReserves));
-        uint256 daiToConvert = (daiObtained.add(fyDaiObtained)).sub(daiToAdd); // Before maturity, so 1 Dai == 1 fyDai in the Controller
-
-        if (daiObtained > daiToConvert) { // Meaning pool1 dai/fyDai > pool2 dai/fyDai. We have a dai surplus that we use to borrow more fyDai.
-            // Convert (daiObtained - daiToConvert) to Chai and use it to borrow more fyDai
-            chai.join(address(this), daiObtained.sub(daiToConvert));
-            // look at the balance of chai in dai to avoid rounding issues
-            uint256 toBorrow = chai.dai(address(this));
-            controller.post(CHAI, address(this), msg.sender, chai.balanceOf(address(this)));
-            controller.borrow(CHAI, fyDai.maturity(), msg.sender, address(this), toBorrow);
-        } else { // Meaning pool1 dai/fyDai < pool2 dai/fyDai. We have a dai shortage so we repay debt with fyDai, withdraw chai collateral, and obtain dai.
-            // Repay (daiToConvert - daiObtained) with fyDai, withdraw the Chai, and unwrap it to Dai
-            uint256 debtToRepay = daiToConvert.sub(daiObtained);
-            uint256 debtInChai = debtToRepay.div(pot.chi());
-            controller.repayFYDai(CHAI, fyDai.maturity(), address(this), msg.sender, debtToRepay);
-            controller.withdraw(CHAI, msg.sender, address(this), debtInChai);
-            chai.exit(address(this), debtInChai);
+        if (daiProportion1 <= daiProportion2) {
+            minted = pool2.mint(address(this), msg.sender, daiObtained);
+            controller.repayFYDai(CHAI, fyDai.maturity(), address(this), msg.sender, fyDai.balanceOf(address(this)));
+        } else {
+            uint256 daiToAdd = fyDaiObtained.div(fyDaiReserves.div(fyDaiReserves.add(daiReserves)));
+            minted = pool2.mint(address(this), msg.sender, daiToAdd);
+            controller.repayDai(CHAI, fyDai.maturity(), address(this), msg.sender, dai.balanceOf(address(this)));
         }
+            
+        withdrawAssets();
+    }
 
-        // mint liquidity tokens
-        return pool2.mint(address(this), msg.sender, daiToAdd);
+    /// @dev Return to caller all posted chai if there is no debt, converted to dai, plus any dai remaining in the contract.
+    function withdrawAssets() internal {
+        uint256 posted = controller.posted(CHAI, msg.sender);
+        uint256 locked = controller.locked(CHAI, msg.sender);
+        require (posted >= locked, "RollProxy: Undercollateralized");
+        controller.withdraw(CHAI, msg.sender, address(this), posted - locked);
+        chai.exit(address(this), chai.balanceOf(address(this)));
+        require(dai.transfer(msg.sender, dai.balanceOf(address(this))), "PoolProxy: Dai Transfer Failed");
     }
 
     /// --------------------------------------------------
     /// Signature method wrappers
     /// --------------------------------------------------
 
-    /// @dev Determine whether all approvals and signatures are in place for `addLiquidity`.
-    /// If `return[0]` is `false`, calling `addLiquidityWithSignature` will set the proxy approvals.
-    /// If `return[1]` is `false`, `addLiquidityWithSignature` must be called with a dai permit signature.
-    /// If `return[2]` is `false`, `addLiquidityWithSignature` must be called with a controller signature.
-    /// If `return` is `(true, true, true)`, `addLiquidity` won't fail because of missing approvals or signatures.
-    function migrateLiquidityCheck(IPool pool1, IPool pool2) public view returns (bool, bool, bool) {
-        /* bool approvals = true;
-        approvals = approvals && chai.allowance(address(this), treasury) == type(uint256).max;
-        approvals = approvals && dai.allowance(address(this), address(chai)) == type(uint256).max;
-        approvals = approvals && dai.allowance(address(this), address(pool)) == type(uint256).max;
-        approvals = approvals && pool.fyDai().allowance(address(this), address(pool)) >= type(uint112).max;
-        bool daiSig = dai.allowance(msg.sender, address(this)) == type(uint256).max;
+    /// @dev Determine whether all approvals and signatures are in place for `migrateLiquidity`.
+    /// If `return[0]` is `false`, calling `migrateLiquidityyWithSignature` will set the proxy approvals.
+    /// If `return[1]` is `false`, `migrateLiquidityWithSignature` must be called with a controller delegation signature.
+    /// If `return[2]` is `false`, `migrateLiquidityWithSignature` must be called with a pool1 delegation signature.
+    /// If `return[3]` is `false`, `migrateLiquidityWithSignature` must be called with a pool2 delegation signature.
+    /// If `return` is `(true, true, true, true)`, `migrateLiquidity` won't fail because of missing approvals or signatures.
+    function migrateLiquidityCheck(IPool pool1, IPool pool2) public view returns (bool, bool, bool, bool) {
+        bool approvals = true;
+        approvals = approvals && dai.allowance(address(this), treasury) == type(uint256).max;
+        approvals = approvals && chai.allowance(address(this), address(chai)) == type(uint256).max;
+        approvals = approvals && dai.allowance(address(this), address(pool2)) == type(uint256).max;
         bool controllerSig = controller.delegated(msg.sender, address(this));
-        return (approvals, daiSig, controllerSig); */
-        return (false, false, false);
+        bool poolSig1 = pool1.delegated(msg.sender, address(this));
+        bool poolSig2 = pool2.delegated(msg.sender, address(this));
+        return (approvals, controllerSig, poolSig1, poolSig2);
     }
 
-    /// @dev Set proxy approvals for `addLiquidity` with a given pool.
-    function migrateLiquidityApprove(IPool pool1, IPool pool2) public {
-        // Allow the Treasury to take chai when posting
-        // if (chai.allowance(address(this), treasury) < type(uint256).max) chai.approve(treasury, type(uint256).max);
+    /// @dev Set proxy approvals for `migrateLiquidity` with a given pool pair.
+    function migrateLiquidityApprove(IPool, IPool pool2) public {
+        // Allow the Treasury to take dai when repaying
+        if (dai.allowance(address(this), treasury) < type(uint256).max) dai.approve(treasury, type(uint256).max);
 
-        // Allow Chai to take dai for wrapping
-        // if (dai.allowance(address(this), address(chai)) < type(uint256).max) dai.approve(address(chai), type(uint256).max);
+        // Allow Chai to take chai for unwrapping
+        if (chai.allowance(address(this), address(chai)) < type(uint256).max) chai.approve(address(chai), type(uint256).max);
 
-        // Allow pool to take dai for minting
-        // if (dai.allowance(address(this), address(pool)) < type(uint256).max) dai.approve(address(pool), type(uint256).max);
-
-        // Allow pool to take fyDai for minting
-        // if (pool.fyDai().allowance(address(this), address(pool)) < type(uint112).max) pool.fyDai().approve(address(pool), type(uint256).max);
+        // Allow pool 2 to take dai for minting
+        if (dai.allowance(address(this), address(pool2)) < type(uint256).max) dai.approve(address(pool2), type(uint256).max);
     }
 
     /// @dev Mints liquidity with provided Dai by borrowing fyDai with some of the Dai.
@@ -144,15 +126,17 @@ contract RollProxy is DecimalMath {
     function migrateLiquidityWithSignature(
         IPool pool1,
         IPool pool2,
-        uint256 poolTokens
+        uint256 poolTokens,
         // uint256 daiUsed,
         // uint256 maxFYDai,
-        // bytes memory daiSig,
-        // bytes memory controllerSig
+        bytes memory controllerSig,
+        bytes memory poolSig1,
+        bytes memory poolSig2
     ) external returns (uint256) {
         migrateLiquidityApprove(pool1, pool2);
-        // if (daiSig.length > 0) dai.permitPackedDai(address(this), daiSig);
-        // if (controllerSig.length > 0) controller.addDelegatePacked(controllerSig);
+        if (controllerSig.length > 0) controller.addDelegatePacked(controllerSig);
+        if (poolSig1.length > 0) pool1.addDelegatePacked(poolSig1);
+        if (poolSig2.length > 0) pool2.addDelegatePacked(poolSig2);
         return migrateLiquidity(pool1, pool2, poolTokens);
     }
 }
